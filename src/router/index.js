64a704f0115fbad5add8eae9b4f6c68304fdccf8
lien0219 <lien0001@163.com>
2024-01-07 21:49:41 +0800

    会员中心

--- src/router/index.js
+++ src/router/index.js
@@ -9,9 +9,9 @@ import CartList from "@/views/CartList/index.vue";
 import Checkout from "@/views/Checkout/index.vue";
 import Pay from "@/views/Pay/index.vue";
 import PayBack from "@/views/Pay/PayBack.vue";
-// import Member from "@/views/Member/index.vue";
-// import MemberInfo from "@/views/Member/components/UserInfo.vue";
-// import MemberOrder from "@/views/Member/components/UserOrder.vue";
+import Member from "@/views/Member/index.vue";
+import MemberInfo from "@/views/Member/components/UserInfo.vue";
+import MemberOrder from "@/views/Member/components/UserOrder.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -51,6 +51,20 @@ const router = createRouter({
         {
           path: "payback",
           component: PayBack,
+        },
+        {
+          path: "member",
+          component: Member,
+          children: [
+            {
+              path: "",
+              component: MemberInfo,
+            },
+            {
+              path: "order",
+              component: MemberOrder,
+            },
+          ],
         },
       ],
     },
